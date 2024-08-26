8efa369f4164ba882a5ec7faf596d14b4f98af5c
Vinh <t.vinh25112003@gmail.com>
2024-08-27 05:02:12 +0700

    update(dev): update dev v4

--- src/main/java/com/ghtk/auction/dto/response/auction/AuctionListResponse.java
+++ src/main/java/com/ghtk/auction/dto/response/auction/AuctionListResponse.java
@@ -8,8 +8,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
@@ -20,24 +18,44 @@ import java.time.LocalDateTime;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class AuctionListResponse {
     Long id;
-    Long product_id;
+    Long productId;
     String title;
     String description;
     String image;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    Timestamp created_at;
+    LocalDateTime createdAt;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    LocalDateTime confirm_date;
+    LocalDateTime confirmDate;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    LocalDateTime end_registration;
+    LocalDateTime endRegistration;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    LocalDateTime start_time;
+    LocalDateTime startTime;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    LocalDateTime end_time;
-    Long start_bid;
-    Long price_per_step;
-    Long end_bid;
+    LocalDateTime endTime;
+    Long startBid;
+    Long pricePerStep;
+    Long endBid;
 
     @Enumerated(EnumType.STRING)
     AuctionStatus status;
+
+    public AuctionListResponse(Long id, Long productId, String title, String description, String image,
+                               LocalDateTime createdAt, LocalDateTime confirmDate, LocalDateTime endRegistration,
+                               LocalDateTime startTime, LocalDateTime endTime, Long startBid, Long pricePerStep,
+                               Long endBid, String status) {
+        this.id = id;
+        this.productId = productId;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.createdAt = createdAt;
+        this.confirmDate = confirmDate;
+        this.endRegistration = endRegistration;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.startBid = startBid;
+        this.pricePerStep = pricePerStep;
+        this.endBid = endBid;
+        this.status = (AuctionStatus.valueOf(status)); // Convert string to Enum
+    }
 }
--- src/main/java/com/ghtk/auction/repository/Custom/impl/AuctionRepositoryCustomImpl.java
+++ src/main/java/com/ghtk/auction/repository/Custom/impl/AuctionRepositoryCustomImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 @Primary
@@ -24,9 +25,11 @@ public class AuctionRepositoryCustomImpl implements AuctionRepositoryCustom {
 
     @Override
     public List<AuctionListResponse> getAllAuctionListResponse(Pageable pageable, AuctionStatus status) {
-        StringBuilder sql = new StringBuilder("SELECT a.id, a.product_id, a.title, a.description, p.image, a.created_at," +
-                " a.confirm_date, a.end_registration, a.start_time, a.end_time, a.start_bid, a.price_per_step, " +
-                "a.end_bid, a.status FROM auction a ");
+        StringBuilder sql = new StringBuilder("SELECT a.id AS id, a.product_id AS productId, a.title AS title, " +
+                "a.description AS description, p.image AS image, a.created_at AS createdAt, a.confirm_date AS confirmDate, " +
+                "a.end_registration AS endRegistration, a.start_time AS startTime, a.end_time AS endTime, " +
+                "a.start_bid AS startBid, a.price_per_step AS pricePerStep, a.end_bid AS endBid, a.status AS status " +
+                "FROM auction a ");
         StringBuilder where = new StringBuilder("WHERE 1=1 ");
         sql.append("join product p on p.id = a.product_id ");
         if(status != null) {
@@ -36,28 +39,9 @@ public class AuctionRepositoryCustomImpl implements AuctionRepositoryCustom {
         sql.append(where);
         sql.append(" LIMIT ").append(pageable.getPageSize()).append("\n");
         sql.append(" OFFSET ").append(pageable.getOffset());
-        Query query = entityManager.createNativeQuery(sql.toString());
-        List<Object[]> results = query.getResultList();
-        List<AuctionListResponse> responses = new ArrayList<>();
-
-        for (Object[] result : results) {
-            AuctionListResponse response = new AuctionListResponse();
-            response.setId((Long) result[0]);
-            response.setProduct_id((Long) result[1]);
-            response.setTitle((String) result[2]);
-            response.setDescription((String) result[3]);
-            response.setImage((String) result[4]);
-            response.setCreated_at(result[5] != null ? ((java.sql.Timestamp) result[5]) : null);
-            response.setConfirm_date(result[6] != null ? ((java.sql.Timestamp) result[6]).toLocalDateTime() : null);
-            response.setEnd_registration(result[7] != null ? ((java.sql.Timestamp) result[7]).toLocalDateTime() : null);
-            response.setStart_time(result[8] != null ? ((java.sql.Timestamp) result[8]).toLocalDateTime() : null);
-            response.setStart_time(result[9] != null ? ((java.sql.Timestamp) result[9]).toLocalDateTime() : null);
-            response.setStart_bid((Long) result[10]);
-            response.setPrice_per_step((Long) result[11]);
-            response.setEnd_bid(result[12] != null ? ((Long) result[12]) : null);
-            response.setStatus(AuctionStatus.valueOf((String) result[13]));
-            responses.add(response);
-        }
-        return responses;
+        Query query = entityManager.createNativeQuery(sql.toString(), "AuctionListProjectionMapping");
+        List<AuctionListResponse> projections = query.getResultList();
+        return projections;
     }
+
 }
